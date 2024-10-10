41dd197865f58118c5e0a3243531b97cf7ae3115
Dyakooo <diako.rudd@outlook.com>
2024-10-10 15:42:44 +1100

    added pubsub

--- function_main.py
+++ function_main.py
@@ -2,14 +2,22 @@ import functions_framework
 from google.cloud import storage
 import json
 import base64
+import logging
+import sys
+
+# Set up logging
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 @functions_framework.cloud_event
 def process_pubsub(cloud_event):
-    data = base64.b64decode(cloud_event.data["message"]["data"]).decode()
-    gold_price_data = json.loads(data)
-    
-    # Process the data (e.g., write to BigQuery)
-    # For now, let's just log it
-    print(f"Received gold price data: {gold_price_data}")
-    
-    return 'OK'
+    logger.debug("Function started")
+    try:
+        data = base64.b64decode(cloud_event.data["message"]["data"]).decode()
+        gold_price_data = json.loads(data)
+        logger.info(f"Received gold price data: {gold_price_data}")
+        # Process the data (e.g., write to BigQuery)
+        return 'OK'
+    except Exception as e:
+        logger.error(f"Error processing message: {str(e)}")
+        raise
