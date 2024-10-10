1ca7914462b785efb85bde23e858a941deda3af8
Dyakooo <diako.rudd@outlook.com>
2024-10-10 12:08:36 +1100

    added min instance to cloud function to fix error

--- cloud_functions.py
+++ cloud_functions.py
@@ -2,8 +2,21 @@ import functions_framework
 import logging
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-@functions_framework.cloud_event
-def process_pubsub(cloud_event):
-    logging.info("Cloud Function triggered successfully!")
+@functions_framework.http
+def process_pubsub(request):
+    """HTTP Cloud Function."""
+    logger.info("Cloud Function triggered successfully!")
     return 'OK', 200
+
+# This is for local testing
+if __name__ == "__main__":
+    from flask import Flask, request
+    app = Flask(__name__)
+    
+    @app.route("/", methods=["POST"])
+    def index():
+        return process_pubsub(request)
+    
+    app.run(host="localhost", port=8080, debug=True)
